93638f633bd9a53bf01abf0ff25a09210ffd9a98
Terashima <yamanogomora@gmail.com>
2019-03-03 09:41:56 +0900

    Add: ViewControllerのソースコードだけで、テーブルを描画する

--- TableViewLesson/ViewController.swift
+++ TableViewLesson/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tableView: UITableView = UITableView(frame: view.frame, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    // UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath[1] + 1)行目"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
